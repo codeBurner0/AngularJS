579c6d7ddaf25ec416e8f71a27ea62b5adcf3a93
CodeBurner <codeburner0@gmail.com>
2023-02-17 16:24:14 +0530

    new files added

--- JS/events.js
+++ JS/events.js
@@ -2,3 +2,13 @@ var app=angular.module("MyMod",[]);
 app.controller("myContro2",function($scope){
     $scope.number=0;
 });
+
+var app=angular.module("MyMod",[]);
+app.controller("myContro2",function($scope){
+    $scope.number=0;
+    $scope.func=function(){
+        $scope.number++;
+    };
+});
+
+
